c49ef200faef4e9019dd6efc6e51cc32d999bae0
teamcutter <ruslan.sibgatulin05@gmail.com>
2025-10-20 20:36:20 +0500

    fix: deleted sensor model

--- internal/model/sensor_data.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type SensorData struct {
-	DeviceID    string  `json:"device_id"`
-	Temperature float64 `json:"temperature"`
-	Humidity    float64 `json:"humidity"`
-	Timestamp   int64   `json:"timestamp"`
-}
